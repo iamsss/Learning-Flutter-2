89cf1c9e279fa9d277041c44587f962e6c7eb1a0
iamsss <saurav9760@gmail.com>
2019-01-27 13:57:08 +0530

    Adding Simple Auth Page

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,7 +1,7 @@
 import 'package:first_app/product_manager.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
