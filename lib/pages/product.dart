3505811f4888e72fd61ce08cd6640d2bbbd92c03
iamsss <saurav9760@gmail.com>
2019-01-27 12:02:20 +0530

    little Design the product page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -8,8 +8,17 @@ class ProductPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Product Details'),
       ),
-      body: Center(
-        child: Text('On the Project Page'),
+      body:  Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('assets/food.jpeg'),
+          Container(padding: EdgeInsets.all(10.0),child: Text('Details'),),
+          RaisedButton(
+            color: Theme.of(context).accentColor,
+            child: Text('BACK'),
+          onPressed: () => Navigator.pop(context),
+          )
+        ]
       ),
     );
   }
