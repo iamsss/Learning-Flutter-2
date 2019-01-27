11d44bc0343ee25ded48977bddf416282497ce89
iamsss <saurav9760@gmail.com>
2019-01-27 11:09:36 +0530

    Navigation from 1 page to another

--- lib/products.dart
+++ lib/products.dart
@@ -1,38 +1,50 @@
+import 'package:first_app/pages/product.dart';
 import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
-    final List<String> products;
+  final List<String> products;
 
-    Products([this.products = const []]);
+  Products([this.products = const []]);
 
-    Widget _buildProductItem(BuildContext context,int index) { // Build product as required
-        return Card(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset('assets/food.jpeg'),
-                              Text(products[index])
-                            ],
-                          ),
-                        
-        );
+  Widget _buildProductItem(BuildContext context, int index) {
+    // Build product as required
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpeg'),
+          Text(products[index]),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Details'),
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => ProductPage())),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductLists() {
+    Widget productCard;
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCard = Container(); // Empty Container Widget like null
     }
+    return productCard;
+  }
 
-    Widget _buildProductLists(){
-       Widget productCard;
-        if(products.length > 0){
-          productCard = ListView.builder ( 
-          itemBuilder: _buildProductItem,
-          itemCount: products.length,
-          );
-        } else {
-          productCard =  Container(); // Empty Container Widget like null
-        }
-        return productCard;
-      
-    }
-
-    @override
-      Widget build(BuildContext context) {
-        return _buildProductLists();
-       }
+  @override
+  Widget build(BuildContext context) {
+    return _buildProductLists();
+  }
 }
